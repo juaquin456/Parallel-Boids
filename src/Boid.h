98543e24d10b3f98c449bd300e19255a87b5b418
juaquin456 <juaquin.remon@utec.edu.pe>
2024-11-12 09:15:45 -0500

    fix: support to offline update

--- src/Boid.h
+++ src/Boid.h
@@ -17,14 +17,28 @@ class Boid {
   Point position;
   Point velocity;
  public:
+  struct Interface {
+    Point idx_position;
+    std::shared_ptr<Boid> boid;
+    explicit Interface(const std::shared_ptr<Boid>& b): boid(b), idx_position(b->position) {}
+    void update() {
+      idx_position = boid->position;
+    }
+    static quadtree::Box<float> getBox(const Boid::Interface& b) {
+      return {b.idx_position.x, b.idx_position.y, 0, 0};
+    }
+    bool operator==(const Boid::Interface& other) const {
+      return idx_position == other.idx_position;
+    }
+  };
+
   Boid(const Point &pos, const Point &vel) : position(pos), velocity(vel) {}
 
-  static quadtree::Box<float> getBox(const std::shared_ptr<Boid>& b) {
-    return {b->position.x, b->position.y, 0, 0};
+  static Interface getInterface(const std::shared_ptr<Boid>& p) {
+    return Interface{p};
   }
-  void updatePosition(const float &timestamp, const quadtree::Quadtree<std::shared_ptr<Boid>, decltype(&Boid::getBox)
-  > &tree) {
-    // complete your code camachito
+  void updatePosition(const float &timestamp, const quadtree::Quadtree<Boid::Interface, decltype(&Boid::Interface::getBox)> &tree) {
+    // TODO: update directly this->position
   }
   void draw(sf::RenderWindow& window) const {
     sf::CircleShape circle(5.);
--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include "Quadtree.h"
 #include "Boid.h"
 
@@ -10,30 +11,40 @@ const unsigned int HEIGHT = 600;
 int main() {
   sf::RenderWindow window(sf::VideoMode(WIDTH, HEIGHT), "Boids");
   vector<shared_ptr<Boid>> boids;
-  for(uint i = 0; i < 100; i++) {
+  vector<Boid::Interface> interfaces;
+  for(int i = 0; i < 100; i++) {
     float x = rand() % WIDTH;
     float y = rand() % HEIGHT;
     boids.emplace_back(make_shared<Boid>(Point{x, y}, Point{1, 0}));
+    interfaces.push_back(Boid::getInterface(boids[i]));
   }
 
   quadtree::Box<float> universe{0., 0, WIDTH, HEIGHT};
-  quadtree::Quadtree<shared_ptr<Boid>, decltype(&Boid::getBox)> boids_tree(universe, Boid::getBox);
+  quadtree::Quadtree<Boid::Interface, decltype(&Boid::Interface::getBox)> boids_tree(universe, Boid::Interface::getBox);
 
-  for (const auto& boid: boids) {
-    boids_tree.add(boid);
+  for (const auto& boidref: interfaces) {
+    boids_tree.add(boidref);
   }
 
-  float step = 2.;
+  sf::Clock clock;
   while (window.isOpen()) {
+    sf::Time elapsed = clock.restart();
     sf::Event event{};
     while (window.pollEvent(event)) {
       if (event.type == sf::Event::Closed) window.close();
     }
+
     for (auto &boid : boids) {
-      boid->updatePosition(step, boids_tree);
+      boid->updatePosition(elapsed.asSeconds(), boids_tree);
     }
-    //boids_tree.update(); or
-    //boids_tree =
+
+    for (auto &boid: interfaces) {
+      boids_tree.remove(boid);
+      boid.update();
+      boids_tree.add(boid);
+    }
+
+    // redraw
     window.clear();
     for (const auto& boid: boids) {
       boid->draw(window);
