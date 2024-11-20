c8bc4c1c578af2d9766fd25d50d13d9800d56f0a
AaronCS25 <aaron.camacho@utec.edu.pe>
2024-11-20 01:58:13 -0500

    chore: add helper class Constants to handle global constants variables

--- src/Boid.h
+++ src/Boid.h
@@ -9,6 +9,7 @@
 #include "Quadtree.h"
 #include "linalg.h"
 #include "SFML/Graphics.hpp"
+#include "Constants.h"
 
 typedef linalg::aliases::float2 Point;
 
@@ -17,18 +18,16 @@ class Boid {
   Point position;
   Point velocity;
 
-  float visual_radius = 100.0f;
+  bool predator = false;
+
+  float visual_radius = 50.0f;
   float protected_radius = 15.0f;
 
-  float avoid_factor = 0.05f;
-  float matching_factor = 0.03f;
-  float centering_factor = 0.0008f;
+  float avoid_factor = 0.01f;
+  float matching_factor = 0.3f;
+  float centering_factor = 0.000002f;
 
   float turn_factor = 0.1f;
-  float margin = 25.0f;
-
-  static constexpr unsigned int WIDTH = 800;
-  static constexpr unsigned int HEIGHT = 800;
 
  public:
 
@@ -51,13 +50,14 @@ class Boid {
     }
   };
 
-  Boid(const Point &pos, const Point &vel) : position(pos), velocity(vel) {}
+  Boid(const Point &pos, const Point &vel, const bool& predator) : position(pos), velocity(vel), predator(predator) {}
 
   static Interface getInterface(const std::shared_ptr<Boid>& p) {
     return Interface{p};
   }
 
   void updatePosition(const float &timestamp, const quadtree::Quadtree<Boid::Interface, decltype(&Boid::Interface::getBox)> &tree) {
+
     float xpos_avg = 0.0f, ypos_avg = 0.0f;
     float xvel_avg = 0.0f, yvel_avg = 0.0f;
     float close_dx = 0.0f, close_dy = 0.0f;
@@ -89,6 +89,12 @@ class Boid {
 
         neighboring_boids++;
       }
+
+      if (neighbor.boid->predator && distance < visual_radius) {
+        close_dx += this->position.x - neighbor.idx_position.x;
+        close_dy += this->position.y - neighbor.idx_position.y;
+      }
+
     }
 
     if (neighboring_boids > 0) {
@@ -105,27 +111,26 @@ class Boid {
     this->velocity.x += close_dx * avoid_factor;
     this->velocity.y += close_dy * avoid_factor;
 
-    if (this->position.x - margin <= 0) {
-        this->position.x = WIDTH - margin;
-    } else if (this->position.x  + margin >= WIDTH) {
-        this->position.x = margin;
-    }
-
-    if (this->position.y -margin <= 0) {
-        this->position.y = HEIGHT - margin;
-    } else if (this->position.y + margin >= HEIGHT) {
-        this->position.y = margin;
-    }
-
-
     this->position.x += this->velocity.x * timestamp;
     this->position.y += this->velocity.y * timestamp;
+
+    if (this->position.x - Constants::MARGIN <= 0) {
+        this->position.x = Constants::WIDTH - Constants::MARGIN;
+    } else if (this->position.x  + Constants::MARGIN >= Constants::WIDTH) {
+        this->position.x = Constants::MARGIN;
+    }
+
+    if (this->position.y - Constants::MARGIN <= 0) {
+        this->position.y = Constants::HEIGHT - Constants::MARGIN;
+    } else if (this->position.y + Constants::MARGIN >= Constants::HEIGHT) {
+        this->position.y = Constants::MARGIN;
+    }
   }
 
   void draw(sf::RenderWindow& window) const {
     sf::CircleShape circle(5.);
     circle.setPosition(position.x, position.y);
-    circle.setFillColor(sf::Color::Green);
+    circle.setFillColor(predator ? sf::Color::Red : sf::Color::Green);
     window.draw(circle);
   }
 };
---
+++ src/Constants.h
@@ -0,0 +1,17 @@
+//
+// Created by aaroncs25 on 11/19/24.
+//
+
+#ifndef HELPERS_H
+#define HELPERS_H
+
+namespace Constants {
+    constexpr int WIDTH = 2000;
+    constexpr int HEIGHT = 2000;
+    constexpr int NUM_THREADS = 12;
+    constexpr int MARGIN = 40;
+    constexpr float FIXED_TIME_STEP = 16.67f;
+    constexpr float TIME_SCALE = 0.2f;
+}
+
+#endif //HELPERS_H
